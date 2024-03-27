6bd6b9db47abbcae7bc8bfbb30c3d05b2d08cfb3
Sammïa Dergent <sammiadergent@hotmail.com>
2024-03-27 09:44:32 +0100

    WOEW de intro buttons werken

--- main.js
+++ main.js
@@ -1,24 +1,20 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// Select all divs and buttons
+const divs = document.querySelectorAll('div[class^="intro_"]');
+const buttons = document.querySelectorAll("button");
 
-setupCounter(document.querySelector('#counter'))
+// Add event listener to each button
+buttons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    // Hide current div
+    if (divs[index]) {
+      divs[index].style.display = "none";
+    }
+
+    // Show next div
+    if (divs[index + 1]) {
+      divs[index + 1].style.display = "block";
+    }
+  });
+});
