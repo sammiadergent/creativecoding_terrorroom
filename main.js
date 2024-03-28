912780d66661d01f2988dbfe92c14ee3615489c9
Sammïa Dergent <sammiadergent@hotmail.com>
2024-03-28 14:14:23 +0100

    test adhd audio

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import "./style.css";
 let introCounter = 0;
 let gamemode = 0;
 let videocounter = 1;
+let audioCounter = 0;
 const videoElement = document.querySelector("#mainVideo");
 const mainvideo = videoElement.querySelector("video");
 const intro = document.querySelector(".intro_0");
@@ -82,18 +83,21 @@ socket.addEventListener("message", (event) => {
         `#failvideo2_${videocounter}`,
       );
       const jumpVideo = jumpscareElement.querySelector("video");
+      let audioFiles = ["audio1.m4a", "audio2.m4a", "audio3.m4a", "audio4.m4a"];
 
       if (jumpVideo) {
         jumpVideo.muted = true;
         jumpVideo.play();
         console.log("Parent div ID: " + jumpVideo.parentNode.id);
-        videocounter++; // Increment videocounter here
+        videocounter = (videocounter + 1) % 5; // Increment videocounter here
 
         jumpVideo.addEventListener("ended", () => {
           jumpscareElement.classList.add("invisable");
           videoElement.classList.remove("invisable");
           mainvideo.muted = true;
           mainvideo.play();
+          audio.play();
+          audioCounter = (audioCounter + 1) % audioFiles.length;
         });
       }
 
